be8fad2098d92b830d8ad948d3cc3cd5032f4d29
rlatjswls14-crypto <rlatjswls14@gmail.com>
2026-02-24 18:52:46 +0900

    Update Program.cs

--- ProductInfo/Program.cs
+++ ProductInfo/Program.cs
@@ -1,19 +1,26 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+ProductInfo productInfo1=new ProductInfo();
+productInfo1.PrintInfo("노트북", 1500000,0);
+ProductInfo productInfo2 = new ProductInfo();
+productInfo2.PrintInfo("키보드", 800000, 20);
+
+
+
 
 
 class ProductInfo
 {
-    public string Name="키보드";
-    public int Price=80000;
-    public int DiscountPercent=0;
+    public string Name{ get; set; }
+    
+    public int Price{ get; set; }
+    public int DiscountPercent { get; set; }
     public int DiscountAmount
     {
         get
         {
-            return Price / DiscountPercent;
+            return DiscountPercent/Price;
         }
     }
     public int FinalPrice
@@ -27,7 +34,7 @@ class ProductInfo
  
 
 
-    public void PrintInfo()
+    public void PrintInfo(string Name, int Price, int DiscountPercent)
     {
         Console.WriteLine($"[상품정보] {Name}- 가격: {Price}원, 할인: {DiscountPercent}%" +
             $"({DiscountAmount}원), 최종가: {FinalPrice}원");
